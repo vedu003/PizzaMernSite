cae7103290d59b484b8bc7123756037b75e3efec
vedu003 <85004625+vedu003@users.noreply.github.com>
2021-07-19 11:43:54 +0530

    some chnages

--- src/app.js
+++ src/app.js
@@ -228,6 +228,14 @@ app.post("/update-cart",(req,res)=> {
     return res.json({ totalQty: req.session.cart.totalQty})
 })
 
+
+app.get("/clearcart",(req,res)=> {
+
+    delete req.session.cart;
+    res.redirect('/cart')
+})
+
+
 app.post("/orders",auth,(req,res)=> {
     
     const {contact,address} = req.body;
